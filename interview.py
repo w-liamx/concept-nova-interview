f88924e5c75d4c0136516b54e91e9c7630ce8743
Williams Afiukwa <62770751+w-liamx@users.noreply.github.com>
2020-10-19 14:08:18 +0100

    Update interview.py

--- interview.py
+++ interview.py
@@ -1,28 +1,38 @@
 a = [2, 3, 1, 19, 18, 12, 7, 18, 22, 32, 20, 11, 8, 5]
+b = [32, 12, 11, 21, 5, 10, 11, 18, 5, 2]
+c = [32, 12, 15, 11, 22, 11, 18, 18, 15, 7]
 
 def checkTrend(a):
     uptrend = []
     downtrend = []
+    index = -1
     returned = {"uptrend": False, "downtrend": False, "index": -1}
     for idx, value in enumerate(a):
-        if idx == 0:
-            uptrend.append(0)
-            downtrend.append(0)
+        if value > (a[idx - 1]):
+            uptrend.append(idx - 1)
+            up_count += 1
+            down_count = 0
+            downtrend.clear()
+            print("uptrend", uptrend)
+            print(downtrend)
         else:
-            if value > (a[idx - 1]):
-                uptrend.append(idx)
-                downtrend.clear()
-            else:
-                downtrend.append(idx)
-                uptrend.clear()
+            downtrend.append(idx - 1)
+            down_count += 1
+            up_count = 0
+            uptrend.clear()
+            print("uptrend",uptrend)
+            print(downtrend)
+
         if len(uptrend) == 3 or len(downtrend) == 3 :
             if len(uptrend) == 3:
                 index = uptrend[0]
             if len(downtrend) == 3:
                 index = downtrend[0]
             break
-    returned['uptrend'] = len(uptrend) == 3
-    returned['downtrend'] = len(downtrend) == 3
+    print("up count: ", up_count)
+    print("down count: ", down_count)
+    returned['uptrend'] = up_count == 3
+    returned['downtrend'] = down_count == 3
     returned['index'] = index
     return returned
     
@@ -30,3 +40,5 @@ def checkTrend(a):
 
 
 print(checkTrend(a))      
+print(checkTrend(b))      
+print(checkTrend(c)) 
